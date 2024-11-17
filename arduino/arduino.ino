c3964a7998ec879b125424d94b3ac91d9f5a8bb1
Ng2k <nicola.ng2k@gmail.com>
2024-11-17 12:56:08 +0100

    refactor(arduino): eseguito riscrittura codice arduino con introduzione classi e classi astratte per rendere il codice il più mantenibile e modulare possibile

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,206 +1,233 @@
-// C++ code
-//
-const byte ledRGBPins[3] = { 11, 9, 10 };
-const byte btnInPin = 2;
-const byte btnOutPin = 4;
+/**
+ * @file arduino.ino
+ * @brief Controllo di un LED RGB tramite pulsanti utilizzando il pattern State.
+ *
+ * Questo programma controlla un LED RGB utilizzando due pulsanti per cambiare il colore del LED
+ * in base allo stato attuale, implementato con il pattern State.
+ *
+ * Per simulare ingressi e uscite è stato usato un LED RGB per rappresentare lo stato:
+ * 		- blu: stato di attesa/idle
+ * 		- verde: ingresso di una persona
+ * 		- rosso: uscita di una persona
+ *
+ * Il programma è stato sviluppato per Arduino Nano 33 IoT e Arduino Mega.
+ *
+ * @version 1.0
+ * @author Nicola Guerra
+ * @author Tommaso Mortara
+ */
 
+#include <Arduino.h>
+
+// Pin del LED RGB
+const byte ledRGBPins[3] = { 11, 9, 10 };
+
+// Pin dei pulsanti
+const byte enterButtonPin = 2;
+const byte exitButtonPin = 4;
+
+// Colori RGB per diversi stati
 const byte idleRGB[3] = { 0, 0, 255 };
 const byte exitRGB[3] = { 255, 0, 0 };
 const byte enterRGB[3] = { 0, 255, 0 };
 
-// frequenza campionamento ingressi
+// Frequenza di campionamento in millisecondi
 const unsigned int FREQUENCY = 1000;
 
-// States:
-enum State {
-	IDLE,
-	READ_IN_IDLE,
-	READ_IN_INCOMPLETE,
-	IN,
-	READ_OUT_IDLE,
-	READ_OUT_INCOMPLETE,
-	OUT
-};
+/**
+ * @enum BtnInput
+ * @brief Stati del pulsante.
+ */
+enum ButtonState { PRESSED, RELEASED };
 
-// inputs:
-enum ButtonInput { PRESSED, RELEASED };
-
-// outputs:
+/**
+ * @enum Output
+ * @brief Stati di output per controllare l'attuatore (LED RGB).
+ */
 enum Output { OFF, ENTER, EXIT };
 
-State iCurrentState;
-ButtonInput iBtnInput;
-ButtonInput iBtnOutput;
-unsigned long lastTime;
-unsigned short crowdCounter;
+/**
+ * @brief Classe astratta per rappresentare uno stato.
+ */
+class State {
+public:
+    virtual void handle(Context* context) = 0;
+};
 
-void setRGBLedColor(const byte rgbPins[3], const byte rgbValues[3])
-{
-	analogWrite(rgbPins[0], rgbValues[0]);
-	analogWrite(rgbPins[1], rgbValues[1]);
-	analogWrite(rgbPins[2], rgbValues[2]);
-}
+/**
+ * @brief Classe contesto che mantiene lo stato corrente.
+ */
+class Context {
+private:
+    State* currentState;
+	ButtonState* enterButtonState;
+	ButtonState* exitButtonState;
+	unsigned short peopleCounter;
+	unsigned long lastExecution;
+public:
+    Context(State* state) : 
+		currentState(state), 
+		enterButtonState(RELEASED), 
+		exitButtonState(RELEASED), 
+		peopleCount(0), 
+		lastExecution(millis()) {}
 
-void setup()
-{
-	for(const byte pin : ledRGBPins) {
-		pinMode(pin, OUTPUT);
+    void setState(State* state) {
+        currentState = state;
+    }
+
+    void request() {
+        currentState->handle(this);
+    }
+
+	/**
+	 * @brief Imposta il colore del LED RGB.
+	 *
+	 * Questa funzione imposta il colore del LED RGB usando i valori RGB forniti.
+	 * Attenzione:	I valori devono essere compresi tra 0 e 255.
+	 *				La funzione, inoltre assume che l'ordine dei valori sia:
+	 *			   	-> [R (Red/Rosso), G (Green/Verde), B (Blue/Blu)].
+	 *
+	 * @param rgbPins Array dei pin del LED RGB.
+	 * @param rgbValues Array dei valori RGB da impostare.
+	 */
+    void setRGBLedColor(const byte rgbValues[3]) {
+        for(unsigned char i = 0; i < 3; i++) {
+            analogWrite(ledRGBPins[i], rgbValues[i]);
+        }
+    }
+};
+
+/**
+ * @brief Classe per rappresentare lo stato di inattività.
+ */
+class IdleState : public State {
+public:
+    void handle(Context* context)
+	{
+		context->setRGBLedColor(idleRGB);
+		context->enterButtonState = RELEASED;
+		context->exitButtonState = RELEASED;
+
+		if(
+			digitalRead(enterButtonPin) == HIGH &&
+			digitalRead(exitButtonPin) == LOW
+		) {
+			context->setState(new ReadInIdleState());
+		}
+		else if(
+			digitalRead(enterButtonPin) == LOW &&
+			digitalRead(exitButtonPin) == HIGH &&
+			context->peopleCount > 0
+		) {
+			context->setState(new ReadOutIdleState());
+		}
 	}
-	
-	pinMode(btnInPin, INPUT);
-	pinMode(btnOutPin, INPUT);
-	
-	iCurrentState = IDLE; // set the initial state
+};
 
-	iBtnInput = RELEASED;
-	iBtnOutput = RELEASED;
+/**
+ * @brief Classe per rappresentare lo stato di lettura inattivo.
+ */
+class ReadInIdleState : public State {
+public:
+	void handle(Context* context)
+	{
+		context->setRGBLedColor(idleRGB);
+		context->enterButtonState = PRESSED;
+		context->exitButtonState = RELEASED;
 
-	crowdCounter = 0;
-	
-	setRGBLedColor(ledRGBPins, idleRGB);
-	
-	Serial.begin(9600);
+		if(
+			digitalRead(enterButtonPin) == LOW &&
+			digitalRead(exitButtonPin) == HIGH
+		) {
+			context->setState(new EnterState());
+			return;
+		}
 
-	lastTime = millis();  
+		context->setState(new IdleState());
+	}
+};
+
+/**
+ * @brief Classe per rappresentare lo stato di lettura inattivo.
+ */
+class ReadOutIdleState : public State {
+public:
+	void handle(Context* context)
+	{
+		context->setRGBLedColor(idleRGB);
+		context->enterButtonState = RELEASED;
+		context->exitButtonState = PRESSED;
+
+		if(
+			digitalRead(enterButtonPin) == HIGH &&
+			digitalRead(exitButtonPin) == LOW &&
+			context->peopleCount > 0
+		) {
+			context->setState(new ExitState());
+			return;
+		}
+
+		context->setState(new IdleState());
+	}
+};
+
+/**
+ * @brief Classe per rappresentare lo stato di ingresso.
+ */
+class EnterState : public State {
+public:
+    void handle(Context* context)
+	{
+		context->setRGBLedColor(enterRGB);
+		context->exitButtonState = PRESSED;
+		context->peopleCount++;
+
+		// todo: chiedere al prof come togliere il delay()
+		delay(1000); 
+		context->setState(new IdleState());
+	}
 }
 
-void loop()
-{
-	if (millis() - lastTime < FREQUENCY) {
+/**
+ * @brief Classe per rappresentare lo stato di uscita.
+ */
+class ExitState : public State {
+public:
+    void handle(Context* context)
+	{
+		context->setRGBLedColor(exitRGB);
+		context->enterButtonState = PRESSED;
+		context->peopleCount--;
+
+ 		// todo: chiedere al prof come togliere il delay()
+		delay(1000);
+		context->setState(new IdleState());
+	}
+};
+
+// Contesto del sistema
+Context* context;
+
+void setup() {
+    for(const byte pin : ledRGBPins) {
+        pinMode(pin, OUTPUT);
+    }
+    pinMode(btnInPin, INPUT);
+    pinMode(btnOutPin, INPUT);
+
+    context = new Context(new IdleState());
+    Serial.begin(9600);
+}
+
+/**
+ * @brief Funzione di loop eseguita continuamente.
+ */
+void loop() {
+	if(context->lastExecution - millis() < FREQUENCY) {
 		return;
 	}
 
-	// Function F (future state)
-	State iFutureState;
-	iFutureState = iCurrentState; // default: same state
-	
-	Serial.println("-----------------------------------------");
-	
-	//Stati: A->A, B->A
-	bool isIdle = (
-		(iCurrentState == IDLE) && (iBtnInput == RELEASED && iBtnOutput == RELEASED)
-	);
-	bool isMultipleIn = (
-		(iCurrentState == READ_IN_IDLE) &&
-		(
-			(iBtnInput == PRESSED && digitalRead(btnInPin) == HIGH) &&
-			iBtnOutput == RELEASED
-		)
-	);
-	bool isMultipleOut = (
-		(iCurrentState == READ_OUT_IDLE) &&
-		(
-			(iBtnOutput == PRESSED && digitalRead(btnOutPin) == HIGH) &&
-			iBtnInput == RELEASED
-		)
-	);
-	
-	if(isIdle || isMultipleIn || isMultipleOut) {
-		iBtnInput = RELEASED;
-		iBtnOutput = RELEASED;
-		iFutureState = IDLE;
-		Serial.println("IDLE");
-	}
-
-	//Stato: A->B
-	if (
-		(iCurrentState == IDLE) &&
-		(digitalRead(btnInPin) == HIGH && iBtnOutput == RELEASED)
-	)
-	{
-		iFutureState = READ_IN_IDLE;
-		iBtnInput = PRESSED;
-		Serial.println("READ_IN_IDLE");
-	}
-	
-	//Stato: B->B
-	if(
-		(iCurrentState == READ_IN_IDLE) &&
-		(iBtnInput == PRESSED && iBtnOutput == RELEASED)
-	)
-	{
-		iFutureState = READ_IN_IDLE;
-		Serial.println("READ_IN_IDLE");
-	}
-	
-	//Stato: B->IN
-	if (
-		(iCurrentState == READ_IN_IDLE) &&
-		(iBtnInput == PRESSED && digitalRead(btnOutPin) == HIGH)
-	)
-	{
-		iFutureState = IN;
-		iBtnOutput = PRESSED;
-		crowdCounter++;
-		Serial.println("IN");
-		Serial.print("crowdCounter: ");
-		Serial.println(crowdCounter);
-	}
-	
-	//Stato IN/OUT->A
-	//todo: frequenza campionamento realistico per evitare 1000 rilevazioni in = 1 out = 1
-	if ((iCurrentState == IN) || (iCurrentState == OUT)) {
-		Serial.println("IDLE");
-		iBtnOutput = RELEASED;
-		iBtnInput = RELEASED;
-		iFutureState = IDLE;
-	}
-	
-	//Stato: A->E
-	if (
-		(iCurrentState == IDLE) &&
-		(iBtnInput == RELEASED && digitalRead(btnOutPin) == HIGH)  &&
-		crowdCounter > 0
-	)
-	{
-		iFutureState = READ_OUT_IDLE;
-		iBtnOutput = PRESSED;
-		Serial.println("READ_OUT_IDLE");
-	}
-	
-	//Stato: E->E
-	if(
-		(iCurrentState == READ_OUT_IDLE) &&
-		(iBtnInput == RELEASED && iBtnOutput == PRESSED)
-	)
-	{
-		Serial.println("READ_OUT_IDLE");
-	}
-	
-	//Stato: E->OUT
-	if (
-		(iCurrentState == READ_OUT_IDLE) &&
-		(digitalRead(btnInPin) == HIGH && iBtnOutput == PRESSED) &&
-		crowdCounter > 0
-	)
-	{
-		iFutureState = OUT;
-		iBtnInput = PRESSED;
-		crowdCounter--;
-		Serial.println("OUT");
-		Serial.print("crowdCounter: ");
-		Serial.println(crowdCounter);
-	}
-
-	// state transition
-	iCurrentState = iFutureState;
-	
-	Output output;
-	if (
-		iCurrentState == IDLE ||
-		iCurrentState == READ_IN_IDLE ||
-		iCurrentState == READ_OUT_IDLE
-	)
-	{
-		output = OFF;
-	}
-	if (iCurrentState == IN) output = ENTER;
-	if (iCurrentState == OUT) output = EXIT;  
-	
-	//  output symbols -> actuators
-	if (output == ENTER) setRGBLedColor(ledRGBPins, enterRGB);  
-	if (output == OFF) setRGBLedColor(ledRGBPins , idleRGB);
-	if (output == EXIT) setRGBLedColor(ledRGBPins , exitRGB);
-
-	lastTime = millis();
+    context->request();
+	context->lastExecution = millis();
 }
