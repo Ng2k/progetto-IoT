c494720b10986a4ef69bee8fd610d53d650ca93a
Ng2k <nicola.ng2k@gmail.com>
2024-11-17 23:29:41 +0100

    feat(raspberry): aggiunto modulo python per leggere dati inviati mediante comunicazione seriale da arduino su raspberry

---
+++ raspberry/python/src/data_printer.py
@@ -0,0 +1,14 @@
+class DataPrinter:
+    """
+    A class responsible for printing the received data.
+    """
+
+    @staticmethod
+    def print_data(data: str) -> None:
+        """
+        Prints the received data.
+
+        Args:
+            data (str): The data to be printed.
+        """
+        print("Received:", data)
