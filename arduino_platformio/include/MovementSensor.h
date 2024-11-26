2cd385db4f00aad00731c9c9bf15d7bc62c1298a
Ng2k <nicola.ng2k@gmail.com>
2024-11-26 16:51:04 +0100

    feat(arduino): refactor progetto usando platformIO

---
+++ arduino_platformio/include/MovementSensor.h
@@ -0,0 +1,49 @@
+#ifndef MOVEMENT_SENSOR_H
+#define MOVEMENT_SENSOR_H
+
+#include "header.h"
+
+class MovementSensor
+{
+    private:
+        const byte _pin;
+		Reading _lastReading;
+
+    public:
+        /**
+        * @brief Costruttore classe del sensore a raggi infrarossi
+        * 
+        * @param pin numero pin dove il sensore è stato inserito
+        */
+        MovementSensor(byte pin);
+
+        /**
+        * @brief Funzione per lettura del pin del sensore
+        *
+        * @return pin del sensore
+        */
+        const byte getPin();
+
+        /**
+         * @brief Funzione per la lettura dello stato del sensore
+         * 
+         * @return stato del sensore
+         */
+        const Reading getLastReading();
+
+        /**
+		 * @brief Legge lo stato del sensore di movimento.
+		 *
+		 * Questa funzione legge lo stato del sensore di movimento,
+		 * lo imposta come stato del sensore e lo restituisce.
+		 */
+		Reading read();
+
+        /**
+         * @brief Aggiorna l'ultimo stato del sensore
+         * 
+         * @param lastReading ultimo stato del sensore
+         */
+        void updateLastState(Reading lastReading);
+};
+#endif //MOVEMENT_SENSOR_H
---
+++ arduino_platformio/src/actuators/LedRgb.cpp
@@ -0,0 +1,20 @@
+#include "LedRGB.h"
+
+LedRgb::LedRgb(const byte pins[3])
+{
+    for (int i = 0; i < 3; ++i) {
+        this->_pins[i] = pins[i];
+        pinMode(this->_pins[i], OUTPUT);
+    }
+};
+
+byte* LedRgb::getPins() { return this->_pins; };
+byte* LedRgb::getRgbValues() { return this->_rgbValues; };
+
+void LedRgb::changeLedColor(const byte rgbValues[3])
+{
+	for(unsigned char i = 0; i < 3; i++) {
+		this->_rgbValues[i] = rgbValues[i];
+		analogWrite(this->_pins[i], rgbValues[i]);
+	}
+}
