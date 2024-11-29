a6e82488c5898d55abcc447efd6094c670fcfc84
Ng2k <nicola.ng2k@gmail.com>
2024-11-29 16:39:10 +0000

    logs(raspberry): aggiunto logs per il componente mqtt del bridge-slave

--- raspberry/bridge_slave/src/communications/bridge/mqtt.py
+++ raspberry/bridge_slave/src/communications/bridge/mqtt.py
@@ -9,34 +9,75 @@ import paho.mqtt.client as mqtt
 
 from .bridge_communication_interface import IBridgeCommunication
 from .mqtt_config import MqttConfig
+from ...log_handler import LogHandler
 
 class MQTTCommunication(IBridgeCommunication):
 	"""
 	Classe per la comunicazione MQTT del bridge slave
 	"""
 
-	def __init__(self, config: MqttConfig):
+	def __init__(self, config: MqttConfig, log_handler: LogHandler):
 		self._config = config
+		self._clientMQTT = None
+		self._log_handler = log_handler
+
 		self.setup()
 			  
 	def setup(self):
-		self._clientMQTT = mqtt.Client()
-		print("Connecting to MQTT broker...")
-		self._clientMQTT.connect(
-			host = self._config.get_broker(),
-			port = self._config.get_port(),
-			keepalive = self._config.get_keepalive()
+		class_name = self.__class__.__name__
+		self._log_handler.log_info(
+			logger="app_logger",
+			message=f"{class_name} - Inizializzazione connessione con il broker MQTT"
 		)
-
-		self._clientMQTT.loop_start()
+		try:
+			self._clientMQTT = mqtt.Client()
+			self._clientMQTT.connect(
+				host = self._config.get_broker(),
+				port = self._config.get_port(),
+				keepalive = self._config.get_keepalive()
+			)
+		except Exception as e:
+			self._log_handler.log_error(
+				logger="critical_logger",
+				message=f"{class_name} - Errore durante la connessione con il broker MQTT",
+				error=e
+			)
+		finally:
+			self._log_handler.log_info(
+				logger="app_logger",
+				message=f"{class_name} - Connessione con il broker MQTT effettuata con successo"
+			)
+			self._log_handler.log_info(
+				logger="app_logger",
+				message=f"{class_name} - Avvio del loop per la ricezione dei messaggi MQTT"
+			)
+			self._clientMQTT.loop_start()
 
 	def publish_data(self, data: dict):
+		class_name = self.__class__.__name__
+
 		pub_topic = self._config.get_pub_topic()
 		mc_id = data.get("mc_id", "")
-		self._clientMQTT.publish(
-			topic = f"{pub_topic}/{mc_id}/people",
-			payload = json.dumps(data)
-		)
+		try:
+			self._log_handler.log_info(
+				logger="app_logger",
+				message=f"{class_name} - Invio dati al topic {pub_topic}/{mc_id}/people"
+			)
+			self._clientMQTT.publish(
+				topic = f"{pub_topic}/{mc_id}/people",
+				payload = json.dumps(data)
+			)
+		except Exception as e:
+			self._log_handler.log_error(
+				logger="critical_logger",
+				message=f"{class_name} - Errore durante l'invio dei dati al topic {pub_topic}/{mc_id}/people",
+				error=e
+			)
+		finally:
+			self._log_handler.log_info(
+				logger="app_logger",
+				message=f"{class_name} - Dati inviati con successo al topic {pub_topic}/{mc_id}/people"
+			)
 
 	def __str__(self):
 		return json.dumps(self._config, indent = 4)
