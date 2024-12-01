b1a0d71884fb3e188657d202d7bd8cf463df6f68
Ng2k <nicola.ng2k@gmail.com>
2024-12-01 09:59:17 +0000

    feat(raspberry): aggiunto colori alle scritte del file start.sh

--- raspberry/start.sh
+++ raspberry/start.sh
@@ -2,76 +2,20 @@
 
 # Reset
 Color_Off='\033[0m'       # Text Reset
+White='[0;37m'        # White
+Black='[0;30m'        # Black
 
 # Regular Colors
-Black='\033[0;30m'        # Black
-Red='\033[0;31m'          # Red
 Green='\033[0;32m'        # Green
 Yellow='\033[0;33m'       # Yellow
-Blue='\033[0;34m'         # Blue
-Purple='\033[0;35m'       # Purple
+Pink='\033[0;35m'         # Pink (instead of Blue)
 Cyan='\033[0;36m'         # Cyan
-White='\033[0;37m'        # White
-
-# Bold
-BBlack='\033[1;30m'       # Black
-BRed='\033[1;31m'         # Red
-BGreen='\033[1;32m'       # Green
-BYellow='\033[1;33m'      # Yellow
-BBlue='\033[1;34m'        # Blue
-BPurple='\033[1;35m'      # Purple
-BCyan='\033[1;36m'        # Cyan
-BWhite='\033[1;37m'       # White
-
-# Underline
-UBlack='\033[4;30m'       # Black
-URed='\033[4;31m'         # Red
-UGreen='\033[4;32m'       # Green
-UYellow='\033[4;33m'      # Yellow
-UBlue='\033[4;34m'        # Blue
-UPurple='\033[4;35m'      # Purple
-UCyan='\033[4;36m'        # Cyan
-UWhite='\033[4;37m'       # White
-
-# Background
-On_Black='\033[40m'       # Black
-On_Red='\033[41m'         # Red
-On_Green='\033[42m'       # Green
-On_Yellow='\033[43m'      # Yellow
-On_Blue='\033[44m'        # Blue
-On_Purple='\033[45m'      # Purple
-On_Cyan='\033[46m'        # Cyan
-On_White='\033[47m'       # White
-
-# High Intensity
-IBlack='\033[0;90m'       # Black
-IRed='\033[0;91m'         # Red
-IGreen='\033[0;92m'       # Green
-IYellow='\033[0;93m'      # Yellow
-IBlue='\033[0;94m'        # Blue
-IPurple='\033[0;95m'      # Purple
-ICyan='\033[0;96m'        # Cyan
-IWhite='\033[0;97m'       # White
-
-# Bold High Intensity
-BIBlack='\033[1;90m'      # Black
-BIRed='\033[1;91m'        # Red
-BIGreen='\033[1;92m'      # Green
-BIYellow='\033[1;93m'     # Yellow
-BIBlue='\033[1;94m'       # Blue
-BIPurple='\033[1;95m'     # Purple
-BICyan='\033[1;96m'       # Cyan
-BIWhite='\033[1;97m'      # White
-
-# High Intensity backgrounds
-On_IBlack='\033[0;100m'   # Black
-On_IRed='\033[0;101m'     # Red
-On_IGreen='\033[0;102m'   # Green
-On_IYellow='\033[0;103m'  # Yellow
-On_IBlue='\033[0;104m'    # Blue
-On_IPurple='\033[0;105m'  # Purple
-On_ICyan='\033[0;106m'    # Cyan
-On_IWhite='\033[0;107m'   # White
+Red='\033[0;31m'          # Red
+BGreen='\033[1;32m'       # Bold Green
+BYellow='\033[1;33m'      # Bold Yellow
+BPink='\033[1;35m'        # Bold Pink
+BCyan='\033[1;36m'        # Bold Cyan
+Gray='\033[0;90m'         # Gray for timestamps
 
 # Settaggio dell'ambiente (default: 'dev')
 ENV=${1:-dev}
@@ -79,27 +23,36 @@ ENV=${1:-dev}
 # Interrompe lo script in caso di errori
 set -e
 
+# Funzione per aggiungere timestamp
+log_with_timestamp() {
+  	echo -e "${White}[${Gray}$(date '+%H:%M:%S')${White}]${Color_Off} $1"
+}
+
 # Configura i permessi per accedere alla porta seriale del microcontrollore
 add_docker_user_to_dialout() {
-  echo "Configurazione dei permessi per la porta seriale..."
-  sudo usermod -aG dialout $USER
-  sudo chmod 666 /dev/ttyACM0
-  echo "Permessi configurati con successo."
+	log_with_timestamp "  |-> ${BYellow}[Command] ${Gray}usermod -aG dialout $USER${Color_Off}"
+	log_with_timestamp "  	|-> ${White}[Description]${Gray} Aggiunge l'utente al gruppo 'dialout' per i permessi alle porte seriali${Color_Off}"
+	sudo usermod -aG dialout $USER
+	sudo chmod 666 /dev/ttyACM0
+	log_with_timestamp "${Green}[Success]${Color_Off} ${BGreen}Permessi configurati con successo.${Color_Off}"
 }
 
 # Esegue docker-compose usando file di environment in base all'input dell'utente
 exec_docker_compose() {
-	echo "Avvio dei container..."
-	echo "	-> Ambiente selezionato: $ENV"
-	echo "	-> Comando eseguito: export DOCKER_BUILDKIT=1 && docker-compose --env-file .env.$ENV -f docker-compose.yml up --no-build -d"
+	log_with_timestamp "${BPink}[Task]${Color_Off} ${Yellow}Avvio dei container tramite ${Cyan}docker-compose${Color_Off}..."
+	log_with_timestamp "${Pink}[Info]${Color_Off} ${Cyan}Ambiente selezionato: ${ENV}${Color_Off}"
+	log_with_timestamp "${Pink}[Info]${Color_Off} ${Cyan}Comando: export DOCKER_BUILDKIT=1 && docker-compose --env-file .env.${ENV} -f docker-compose.yml up --no-build -d${Color_Off}"
 	export DOCKER_BUILDKIT=1 && docker-compose --env-file .env.$ENV -f docker-compose.yml up --no-build -d
-	echo "Container avviati con successo."
+	log_with_timestamp "${Green}[Success]${Color_Off} ${BGreen}Container avviati con successo. Verifica i log tramite ${Cyan}docker-compose logs${Color_Off}.${Color_Off}"
 }
 
 # Main script
 main() {
+	log_with_timestamp "${Pink}[Info]${Color_Off} ${White}Script di configurazione e avvio dei container Docker.${Color_Off}"
+	log_with_timestamp "${BPink}[Start]${Color_Off} ${White}Inizio configurazione e avvio container.${Color_Off}"
 	add_docker_user_to_dialout # Configura i permessi per accedere alla porta seriale del microcontrollore
-	exec_docker_compose # Esegue docker-compose usando file di environment in base all'input dell'utente
+	exec_docker_compose       # Esegue docker-compose usando file di environment in base all'input dell'utente
+	log_with_timestamp "${Green}[Done]${Color_Off} ${BGreen}Script completato con successo!${Color_Off}"
 }
 
 main
