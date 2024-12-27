913fe12b0489d122f1ba0fc2b89916c8256a4504
Ng2k <nicola.ng2k@gmail.com>
2024-12-27 19:32:17 +0100

    fix(arduino): tolti dei print di debug

--- arduino/src/states/ErrorState.cpp
+++ arduino/src/states/ErrorState.cpp
@@ -1,7 +1,6 @@
 #include "State.h"
 
 void ErrorState::handle(Context* ctx) {
-	Serial.("Error");
 	ctx->setContextOutput(Output::Error);
 	this->_lastTime = millis();
 	ctx->setContextState(new IdleState());
