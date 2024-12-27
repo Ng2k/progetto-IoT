a85fb118df138d089ed52c46cc647bb1c6444593
Ng2k <nicola.ng2k@gmail.com>
2024-12-28 00:04:20 +0100

    fix(arduino): sistemato codice per display lcd

--- arduino/display_lcd/src/main.cpp
+++ arduino/display_lcd/src/main.cpp
@@ -4,19 +4,17 @@
 
 LiquidCrystal_I2C lcd(0x27,20,4);
 
-const byte sda = 20;
-const byte scl = 21;
-
-const byte sda_esp32 = 16;
-const byte scl_esp32 = 4;
+bool i2CAddrTest(uint8_t addr);
 
 void setup() {
-	Wire.begin(sda_esp32, scl_esp32);
-	lcd.init();
-	lcd.backlight();
-	lcd.clear();
-	lcd.setCursor(0, 0);
-	Serial.begin(9600);
+	Wire.begin(SDA, SCL); // attach the IIC pin
+	if (!i2CAddrTest(0x27)) {
+		lcd = LiquidCrystal_I2C(0x3F, 16, 2);
+	}
+	lcd.init(); // LCD driver initialization
+	lcd.backlight(); // Open the backlight
+	lcd.setCursor(0,0); // Move the cursor to row 0, column 0
+	lcd.print("hello world"); // The print content is displayed
 }
 
 void loop() {
@@ -48,4 +46,13 @@ void loop() {
 			}
 		}
 	}
+}
+
+bool i2CAddrTest(uint8_t addr) {
+	Wire.begin();
+	Wire.beginTransmission(addr);
+	if (Wire.endTransmission() == 0) {
+		return true;
+	}
+	return false;
 }
